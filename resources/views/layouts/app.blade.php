1f97a0a4295f976b7e783d9ce0ed8678814da1a5
Edgaras Avagyan <35609247+Crownley@users.noreply.github.com>
2020-08-22 20:36:07 +0300

    Doublechecking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,6 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
